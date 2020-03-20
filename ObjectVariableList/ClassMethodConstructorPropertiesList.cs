f446b0286cda7ac6fd361426fea79ca57ed8e865
Олександр Кухар <redlinerailmetro@gmail.com>
2020-03-20 20:11:38 +0200

    Добавьте файлы проекта.

---
+++ ObjectVariableList/ClassMethodConstructorPropertiesList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*Задание
+1) Создать класс
+ - 3 свойства
+ - 1 метод
+ - конструктор для свойств
+2) Создать переменную типа List<%your_class_name%>
+3) Для этой переменной:
+ - добавить 1 обьект
+ - добавить набор обьектов
+ - удалить нужный обьект
+ - удалить по индексу*/
+namespace ObjectVariableList
+{
+    class ClassMethodConstructorPropertiesList
+    {
+        public string clas { get; set; }
+        public int method { get; set; }
+        public decimal properties { get; set; }
+
+        public static string method_NoLoveCorona()
+        {
+            return "No love carantin ";
+        }
+        public ClassMethodConstructorPropertiesList(string clas, int method, decimal properties)
+        {
+            this.clas = clas;
+            this.method = method;
+            this.properties = properties;
+        }
+
+        public static explicit operator string(ClassMethodConstructorPropertiesList v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ObjectVariableList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+ * Задание
+1) Создать класс
+ - 3 свойства
+ - 1 метод
+ - конструктор для свойств
+2) Создать переменную типа List<%your_class_name%>
+3) Для этой переменной:
+ - добавить 1 обьект
+ - добавить набор обьектов
+ - удалить нужный обьект
+ - удалить по индексу
+ */
+namespace ObjectVariableList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassMethodConstructorPropertiesList classMethodConstructorPropertiesList = new ClassMethodConstructorPropertiesList("mama mia", 29, 16);
+            List<string> carantino_list = new List<string>();
+            
+            carantino_list.Add("Full trash");
+           // carantino_list.AddRange((string)classMethodConstructorPropertiesList);
+            carantino_list.AddRange(new string [] { "dont like", "not alike", "hate carantin" });
+            carantino_list.RemoveAt(0);
+            carantino_list.Remove("Epic Fail");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
